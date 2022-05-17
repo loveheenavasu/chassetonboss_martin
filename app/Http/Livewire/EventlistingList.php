b0c881c637ed6e1179155d97077652af9b1dc682
loveheenavasu <trivediheenalove@gmail.com>
2022-05-17 11:59:38 +0000

    fixed the csv issue

--- app/Http/Livewire/EventlistingList.php
+++ app/Http/Livewire/EventlistingList.php
@@ -13,13 +13,151 @@ class EventlistingList extends Component
 {
     use WithPagination;
     public bool $confirmingListingDeletion = false;
+    public bool $confirmingReset = false;
+    public bool $confirmingClone = false;
     public ?EventListing $listingBeingDeleted = null;
     public $checkid = '';
+    public $resetid='';
+    public $cloneid='';
+    public $lastcloeid = '';
+    public $allCloneEmails = [];
 
     public function getEventlistingsProperty()
     {
         return EventListing::query()->paginate(100);
     }
+
+    /**************** Reset Function *********/
+    public function confirmingReset(EventListing $eventlisting): void
+    {
+        $this->confirmingReset = true;
+        $this->listingBeingDeleted = $eventlisting;
+        $this->resetid = $eventlisting->id;
+    }
+
+
+    public function resetConnection(): void
+    {
+        $emailIds=[];
+        $eventEmail=[];
+        $evenListingEmail = DB::table('eventlisting_emails')->where('event_listing_id',$this->resetid)->where('in_pool',0)->get();
+        if(!$evenListingEmail->isEmpty()){
+            foreach($evenListingEmail as $email){
+              $emailIds[] = $email->event_email_id;
+            }
+        }else{
+            $this->confirmingReset = false;
+        }
+        if(!empty($emailIds)){
+            $event_email=DB::table('eventemails')->whereIn('id',$emailIds)->get();
+            if(!$event_email->isEmpty()){
+              foreach($event_email as $e_mail){
+                $eventEmail[]=$e_mail->email;
+              }
+            }
+        }
+        $new_id_array=[];
+        if(!empty($eventEmail)){
+            $event_email=DB::table('eventemails')->whereIn('email',$eventEmail)->get()->toArray();
+            if(!empty($event_email)){
+              foreach($event_email as $unique_mail){
+                $new_id_array[]=$unique_mail->id;
+              }
+            }
+        }
+        if(!empty($new_id_array)){
+            DB::table('eventlisting_emails')->whereIn('event_email_id',$new_id_array)->where('event_listing_id',$this->resetid)->update(['in_pool'=>1]);
+            $this->confirmingReset = false;
+        }
+    }
+
+    /****************Reset Function End *********/
+
+
+    /****************Clone Function Start *********/
+    public function confirmingClone(EventListing $eventlisting): void
+    {
+        $this->confirmingClone = true;
+        $this->listingBeingDeleted = $eventlisting;
+        $this->cloneid = $eventlisting->id;
+    }
+
+    public function cloneConnection(): void
+    {
+        $event_id = $this->cloneid;
+        $event = DB::table('eventlistings')->where('id',$this->cloneid)->get()->toArray();
+        $all_emails = DB::table('event_invalid_emails')->pluck('email')->toArray();
+        $allValidEmails=[];
+        $allVaildEmailIds=[];
+        $evenListingEmail = DB::table('eventemails')->where('rule_id',$this->cloneid)->pluck('email')->toArray();
+        if(!empty($evenListingEmail)){
+            foreach($evenListingEmail as $email){
+                if (!in_array($email, $all_emails)){
+                    $allValidEmails[] = $email;
+                }
+            }
+        }
+        if(!empty($allValidEmails)){
+            $allVaildEmailIds = DB::table('eventemails')->whereIn('email',$allValidEmails)->pluck('id')->toArray();
+        }
+        if(!empty($allVaildEmailIds)){
+            DB::table('eventlisting_emails')->whereIn('event_email_id',$allVaildEmailIds)->where('event_listing_id',$this->cloneid)->update(['in_pool'=>1]);
+            $event_new_id = $this->cloneEvent();
+            $this->clonEmails($allValidEmails);
+            $this->cloneEventemail($event_new_id);
+            $this->confirmingClone = false;
+        }
+
+    }
+    public function cloneEvent()
+    {
+        $new = EventListing::find($this->cloneid);
+        $new = $new->replicate();
+        $new->name = $new->name . ' (Copy)';
+        $new->save();
+        $this->lastcloeid = $new->id;
+        return $new->id;
+    }
+
+
+    public function clonEmails($allValidEmails)
+    {
+        $list=[];
+        foreach($allValidEmails as $allValidEmail){
+            $insertEmails = array('email' => $allValidEmail, 'sync_status' => 'no', 'rule_id' => $this->lastcloeid,'created_at' => date('Y-m-d H:i:s'),'updated_at' => date('Y-m-d H:i:s'));
+            $this->allCloneEmails[] = DB::table('eventemails')->insertGetId($insertEmails);
+        }
+    }
+
+    public function cloneEventemail($event_id)
+    {
+        $list=[];
+        foreach($this->allCloneEmails as $emailid){
+            $listEmails = array('event_listing_id' => $event_id,'event_email_id' => $emailid);
+            DB::table('eventlisting_emails')->insert($listEmails);
+        }
+    }
+
+    /****************clone function end *********/
+
+    public function exportInPollList(EventListing $eventlisting){
+        if (!$eventlisting->id) {
+            return;
+        }
+        return response()->streamDownload(function () use ($eventlisting) {
+            echo $eventlisting->copiedValue();
+        }, '' . $eventlisting->name . '.csv');
+    }
+
+    public function exportNotInPollList(EventListing $eventlisting){
+        if (!$eventlisting->id) {
+            return;
+        }
+        return response()->streamDownload(function () use ($eventlisting) {
+            echo $eventlisting->copiedValue();
+        }, '' . $eventlisting->name . '.csv');
+    }
+
 
     public function confirmingListingDeletion(EventListing $eventlisting): void
     {
@@ -28,7 +166,9 @@ class EventlistingList extends Component
         $this->checkid = $eventlisting->id;
     }
 
-    public function deleteConnection(): void
+
+
+    public function deleteEventList(): void
     {
         $all= EventListing::find($this->checkid);
         if(!empty($all->id)){
@@ -39,26 +179,8 @@ class EventlistingList extends Component
             EventEmailInfo::where('event_email_id',$value)->delete();
         }
         DB::table('eventlisting_emails')->where('event_listing_id',$all->id)->delete();
+
         $this->listingBeingDeleted->delete();
-
         $this->confirmingListingDeletion = false;
-    }
-
-    public function exportInPollList(EventListing $eventlisting){
-        if (!$eventlisting->id) {
-            return;
-        }
-        return response()->streamDownload(function () use ($eventlisting) {
-            echo $eventlisting->copiedValue();
-        }, '' . $eventlisting->name . '.csv');        
-    }
-
-    public function exportNotInPollList(EventListing $eventlisting){
-        if (!$eventlisting->id) {
-            return;
-        }
-        return response()->streamDownload(function () use ($eventlisting) {
-            echo $eventlisting->copiedValue();
-        }, '' . $eventlisting->name . '.csv');        
     }
 }
--- resources/views/livewire/eventlisting-list.blade.php
+++ resources/views/livewire/eventlisting-list.blade.php
@@ -27,13 +27,20 @@
                                 <button class="cursor-pointer ml-6 text-sm text-gray-500 focus:outline-none" wire:click="exportInPollList({{ $listing->id}})">
                                     {{ __('Export Used') }}
                                 </button>
-                                
+
                                 <a href="{{ route('eventlistings.show', ['eventlisting' => $listing->id]) }}" class="cursor-pointer ml-6 text-sm text-gray-400 focus:outline-none">
                                     {{ __('Details') }}
                                 </a>
+                                <button class="cursor-pointer ml-6 text-sm text-red-500 focus:outline-none" wire:click="confirmingReset({{ $listing->id}})">
+                                    {{ __('Reset') }}
+                                </button>
+                                <button class="cursor-pointer ml-6 text-sm text-green-500 focus:outline-none" wire:click="confirmingClone({{ $listing->id}})">
+                                    {{ __('Clone') }}
+                                </button>
                                 <button class="cursor-pointer ml-6 text-sm text-red-500 focus:outline-none" wire:click="confirmingListingDeletion({{ $listing->id}})">
                                     {{ __('Delete') }}
                                 </button>
+
                             </div>
                         </div>
                     @endforeach
@@ -66,9 +73,56 @@
                 {{ __('Nevermind') }}
             </x-jet-secondary-button>
 
-            <x-jet-danger-button class="ml-2" wire:click="deleteConnection" wire:loading.attr="disabled">
+            <x-jet-danger-button class="ml-2" wire:click="deleteEventList" wire:loading.attr="disabled">
                 {{ __('Delete') }}
             </x-jet-danger-button>
         </x-slot>
     </x-jet-confirmation-modal>
+    <!-----reset modal--->
+    <x-jet-confirmation-modal wire:model="confirmingReset">
+        <x-slot name="title">
+            {{ __('Reset List') }}
+        </x-slot>
+
+        <x-slot name="content">
+            @if($this->listingBeingDeleted)
+                {{ __('Are you sure you want to reset this :name?', ['name' => $this->listingBeingDeleted->name]) }}
+            @endif
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('confirmingReset')" wire:loading.attr="disabled">
+                {{ __('Nevermind') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="resetConnection" wire:loading.attr="disabled">
+                {{ __('Reset') }}
+            </x-jet-danger-button>
+
+        </x-slot>
+    </x-jet-confirmation-modal>
+    <!-----clone modal--->
+    <x-jet-confirmation-modal wire:model="confirmingClone">
+        <x-slot name="title">
+            {{ __('Clone List') }}
+        </x-slot>
+
+        <x-slot name="content">
+            @if($this->listingBeingDeleted)
+                {{ __('Are you sure you want to clone this :name?', ['name' => $this->listingBeingDeleted->name]) }}
+            @endif
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('confirmingClone')" wire:loading.attr="disabled">
+                {{ __('Nevermind') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="cloneConnection" wire:loading.attr="disabled">
+                {{ __('Clone') }}
+            </x-jet-danger-button>
+
+        </x-slot>
+    </x-jet-confirmation-modal>
+
 </div>
